b8c088cb839e7d0fbcedfa78081867f86765dd79
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    marble: Add Xiaomi sensor module
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -39,7 +39,10 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    marbleLightSensor
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
