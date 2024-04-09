0c5110b918ba510330a18c7fcbc64ada905d271e
SebaUbuntu <seba@sebaubuntu.dev>
2024-04-10 04:21:20 +0900

    marble: Import libinit from old tree
    Co-Authored-By: Ramii Ahmed <ramy@ahmedramy.com>
    Co-Authored-By: luk1337 <priv.luk@gmail.com>
    Co-Authored-By: Arian <arian.kulmer@web.de>
    Co-Authored-By: Mohammed Althaf Thayyil <me@althafvly.com>
    Co-Authored-By: YuKongA <70465933+YuKongA@users.noreply.github.com>

--- mist_marble.mk
+++ mist_marble.mk
@@ -11,20 +11,11 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 $(call inherit-product, vendor/mist/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := marble
-PRODUCT_MODEL := 23049PCD8G
 PRODUCT_NAME := mist_marble
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := marble_global
-PRODUCT_SYSTEM_DEVICE := marble
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.8.0.UMREUXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.8.0.UMREUXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
